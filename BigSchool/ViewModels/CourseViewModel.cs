97ceeb9705b459307b5af7981dd893923730d107
ADMIN <ADMIN@LAPTOP-I7A8S1NV>
2022-06-10 00:42:30 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace bigschool.ViewModels
+{
+    public class CourseViewModel
+    {   [Required]
+        public string Place { get; set; }
+        [Required]
+        [FuturaDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
---
+++ ViewModels/FuturaDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class FuturaDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime DateTime;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "dd/M/yyyy",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out DateTime);
+            return (isValid && DateTime > DateTime.Now);
+        }
+    }
+}
